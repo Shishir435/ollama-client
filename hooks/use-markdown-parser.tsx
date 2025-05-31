2d754493d121fcd13d03ead27f1968ddc883babf
Shishir435 <shishirchaurasiya435@gmail.com>
2025-05-31 17:44:25 +0530

    feat: update markdown parser with additional plugins for enhanced functionality

--- hooks/use-markdown-parser.tsx
+++ hooks/use-markdown-parser.tsx
@@ -3,9 +3,13 @@ import hljs from "highlight.js"
 import MarkdownIt from "markdown-it"
 import container from "markdown-it-container"
 import MarkdownItCopyCode from "markdown-it-copy-code"
+import deflist from "markdown-it-deflist"
 import { full as emoji } from "markdown-it-emoji"
 import footnote from "markdown-it-footnote"
 import highlightjs from "markdown-it-highlightjs"
+import markdownItMark from "markdown-it-mark"
+import sub from "markdown-it-sub"
+import sup from "markdown-it-sup"
 import taskLists from "markdown-it-task-lists"
 import { useEffect, useState } from "react"
 
@@ -34,6 +38,10 @@ md.use(highlightjs, { hljs })
   .use(container, "warning")
   .use(emoji)
   .use(MarkdownItCopyCode)
+  .use(markdownItMark)
+  .use(deflist)
+  .use(sub)
+  .use(sup)
 
 export function useMarkdownParser(markdown: string) {
   const [html, setHtml] = useState("")
