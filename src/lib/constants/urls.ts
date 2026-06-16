477e0b8b123c78f9aff3f8008cad01bcceefe201
Shishir435 <shishirchaurasiya435@gmail.com>
2026-06-16 22:01:49 +0530

    docs: update docs domain

--- src/lib/constants/urls.ts
+++ src/lib/constants/urls.ts
@@ -2,7 +2,7 @@
  * External URLs used throughout the extension.
  * Keep all hardcoded links here so they can be updated in one place.
  */
-const DEFAULT_DOCS_HOME = "https://ollama-client.shishirchaurasiya.in"
+const DEFAULT_DOCS_HOME = "https://www.ollamaclient.in"
 const DOCS_HOME = DEFAULT_DOCS_HOME.replace(/\/+$/, "")
 
 export const EXTERNAL_URLS = {
